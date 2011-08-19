ea612dac0a5eea4617ae399670e23bc4aea816f7
Jim Rodgers <jim@meebo-inc.com>
2011-08-19 14:10:13 -0700

    Fix missing local variables in threaded file loader, fix possible race conditions and non-thread-safe behavior.
    The thread load_file function needed access to the reloader object,
    which was a local variable in the function that set up the thread.
    However, I think there's a possibility of race conditions and non-thread
    safe activity if we update shared objects from inside the thread.  I
    changed the work in the thread to collect everything in objects on the
    class (and turned the thread in a Thread subclass).  When the thread is
    finished running, we pull the loaded mounts out of it.  I also deferred
    the server hooks until then because I'm not sure the author of the hooks
    might not expect them to be run in a thread.
    
    I also believe there's a potential race condition that the threads might
    not be done executing by the time we were moving on to creating the
    Router() object.  So we now wait up to 5 seconds for the threads to
    finish before proceeding to use the result.

--- greins/app.py
+++ greins/app.py
@@ -4,6 +4,7 @@ import os.path
 import sys
 import textwrap
 import threading
+import time
 import traceback
 
 from gunicorn.app.wsgiapp import WSGIApplication
@@ -63,69 +64,49 @@ class GreinsApplication(WSGIApplication):
                 exec hook_proxy_template % {'spec': spec} in proxy_env
                 self.cfg.set(name, proxy_env['proxy'])
 
-    def load_file(self,cf):
-        cf_name = os.path.splitext(os.path.basename(cf))[0]
-        cfg = {
-            "__builtins__": __builtins__,
-            "__name__": "__config__",
-            "__file__": os.path.abspath(cf),
-            "__doc__": None,
-            "__package__": None,
-            "mounts": {}
-        }
-        try:
-            """
-            Read an app configuration from a greins config file.
-            Files should contain app handlers with mount points
-            for one or more wsgi applications.
-
-            The handlers will be executed inside the environment
-            created by the configuration file.
-            """
-            self.logger.info("Loading configuration for %s" % cf_name)
-            execfile(cf, cfg, cfg)
-
-            # Load all the mount points
-            for r, a in cfg['mounts'].iteritems():
-                # Capture the handler in a closure
-                def wrap(app):
-                    def app_with_env(env, start_response):
-                        return app(env, start_response)
-                    app_with_env.__name__ = app.__name__
-                    return app_with_env
-                wrapped = wrap(a)
-                if not r.startswith('/'):
-                    self.logger.warning("Adding leading '/' to '%s'" % r)
-                    r = '/' + r
-                if self._mounts.setdefault(r, wrapped) != wrapped:
-                    self.logger.warning("Duplicate routes for '%s'" % r)
-                    continue
-
-            # Set up server hooks
-            for hook in self._hooks:
-                handler = cfg.get('%s' % hook)
-                if handler:
-                    self._hooks[hook]['validator'](handler)
-                    self._hooks[hook]['handlers'].append(handler)
-        except Exception, e:
-            if self._use_reloader:
-                for fname, _, _, _ in traceback.extract_tb(sys.exc_info()[2]):
-                     reloader.extra_files.add(fname)
-                if isinstance(e, SyntaxError):
-                     reloader.extra_files.add(e.filename)
-            self.logger.exception("Exception reading config for %s:" % \
-                                      cf_name)
-
     def load(self):
         if self._use_reloader:
             reloader = Reloader()
+
+        threads = []
         for cf in glob.glob(os.path.join(self.app_dir, '*.py')):
             # isolate config loads on different threads (or greenlets if
             # this is a gevent worker).  If one of the apps fails to
             # start cleanly, the other apps will still function
             # properly.
-            t = threading.Thread(target=self.load_file, args=[cf])
+            t = FileLoader(cf, self._use_reloader, self.logger)
+            threads.append(t)
             t.start()
+
+        # wait up to approximately 5 seconds for threads to finish
+        max_wait = 5
+        start_time = time.time()
+        while [t for t in threads if t.isAlive()] and (time.time() - start_time) < max_wait :
+            time.sleep(0.01)
+
+        for t in threads:
+            # thread finished successfully
+            if not t.is_alive():
+                # pull the loaded mount points out of the thread
+                for route, value in t.mounts.iteritems():
+                    if self._mounts.setdefault(route, value) != value:
+                        self.logger.warning("Duplicate routes for '%s'", route)
+
+                # Set up server hooks
+                for hook in self._hooks:
+                    handler = t.cfg.get('%s' % hook)
+                    if handler:
+                        try:
+                            self._hooks[hook]['validator'](handler)
+                            self._hooks[hook]['handlers'].append(handler)
+                        except Exception, e:
+                            self.logger.exception("Error setting up hooks for file %s", t.cf)
+
+                if self._use_reloader:
+                    reloader.extra_files.union(t.reloader_extra_files)
+            # thread didn't finish.  not much we can do except ignore it
+            else:
+                self.logger.error("file failed to load: %s", t.cf())
 
         if self._use_reloader:
             reloader.start()
@@ -137,6 +118,67 @@ class GreinsApplication(WSGIApplication):
     def _do_hook(self, name, argtuple):
         for handler in self._hooks[name]['handlers']:
             handler(*argtuple)
+
+
+class FileLoader(threading.Thread):
+    def __init__(self, cf, use_reloader, logger):
+        threading.Thread.__init__(self)
+
+		# config file to read from
+        self.cf = cf
+        self._use_reloader = use_reloader
+		# collect list of files to reload, if using reloader
+        self.reloader_extra_files = set()
+        self.logger = logger
+		# collect mounts specified in config file
+        self.mounts = {}
+        self.cfg = {
+            "__builtins__": __builtins__,
+            "__name__": "__config__",
+            "__file__": os.path.abspath(self.cf),
+            "__doc__": None,
+            "__package__": None,
+            "mounts": {}
+        }
+
+    def run(self):
+        cf_name = os.path.splitext(os.path.basename(self.cf))[0]
+        try:
+            """
+            Read an app configuration from a greins config file.
+            Files should contain app handlers with mount points
+            for one or more wsgi applications.
+
+            The handlers will be executed inside the environment
+            created by the configuration file.
+            """
+            self.logger.info("Loading configuration for %s" % cf_name)
+            execfile(self.cf, self.cfg, self.cfg)
+
+            # Load all the mount points
+            for r, a in self.cfg['mounts'].iteritems():
+                # Capture the handler in a closure
+                def wrap(app):
+                    def app_with_env(env, start_response):
+                        return app(env, start_response)
+                    app_with_env.__name__ = app.__name__
+                    return app_with_env
+                wrapped = wrap(a)
+                if not r.startswith('/'):
+                    self.logger.warning("Adding leading '/' to '%s'" % r)
+                    r = '/' + r
+                if self.mounts.setdefault(r, wrapped) != wrapped:
+                    self.logger.warning("Duplicate routes for '%s'" % r)
+                    continue
+
+        except Exception, e:
+            if self._use_reloader:
+                for fname, _, _, _ in traceback.extract_tb(sys.exc_info()[2]):
+                     self.reloader_extra_files.add(fname)
+                if isinstance(e, SyntaxError):
+                     self.reloader_extra_files.add(e.filename)
+            self.logger.exception("Exception reading config for %s:" % \
+                                      cf_name)
 
 def run():
     """\
